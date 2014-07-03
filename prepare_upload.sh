b5e0032115b921fc51ad66de60042e6b78c3ce0c
Jason Liu <jason@jasonkliu.com>
2014-07-03 13:09:28 -0700

    New upload script and exclude from _config.yml

---
+++ prepare_upload.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Rename files to prepare for upload
+# Make sure everything is checked into Git first!
+# Only use this on OSX with default sed, sed works differently elsewhere!
+
+echo "Script made by Jason Liu, July 2014"
+echo ""
+
+git --version 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+  echo "Git is not available on your system. Please install it."
+  read
+fi
+
+orig="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Running in: $orig"
+echo ""
+
+echo "Files to replace: "
+grep -riInl "jasonkliu.github.io" * --exclude-dir=_site --exclude=*.sh
+
+sed -i '' 's/jasonkliu.github.io/yale.edu/' index.md
+sed -i '' 's/jasonkliu.github.io/yale.edu/' _config.yml
+sed -i '' 's/jasonkliu.github.io/yale.edu/' _includes/head.html
+
